b5053fd04fb0d51187c9ea3642172d3bdc29496d
only4cc <only4cc@hotmail.com>
2018-10-24 17:41:32 -0300

    Comentarios

--- check.pl
+++ check.pl
@@ -1,10 +1,10 @@
 # Verificacion basica de Nodos del Cluster 
 my $DEBUG = 0;
 
-my @IPS=('192.168.171.63', '192.168.171.64', '192.168.171.65',
-         '192.168.161.63', '192.168.161.64', '192.168.161.65' );
+my @IPS=('192.168.X1.Y1', '192.168.X1.Y2', '192.168.X1.Y3',
+         '192.168.X2.Y1', '192.168.X2.Y2', '192.168.X2.Y3' );
 
-my $cluster_name = shift || 'BE0ORCHP-CASS0C';
+my $cluster_name = shift || 'NOMBRE_DEL_CLUSTER';  # <<< Ajustar al nombre del Cluster a Monitorear
 my $CMD;
 my $tot_ok = 0;
 foreach my $ip ( @IPS ) {
